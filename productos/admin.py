128f22036ce48409db13052b178dbd331d9ccf05
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-11-27 19:31:44 -0300

    edito admin

--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Producto
+
+@admin.register(Producto)
+
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'stock', 'categoria', 'disponible')
